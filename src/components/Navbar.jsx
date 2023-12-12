b7430f6807208447338fd7e301990399c3a5b1ae
Ruben M. Abreu <ruben_abreu@icloud.com>
2023-12-12 21:58:51 +0000

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <header className='header'>
+      <nav>
+        <div className='logo'>
+          <Link to='#home'>
+            {`<Ruben`}
+            <span>{`Abreu/>`}</span>
+          </Link>
+        </div>
+        <input type='checkbox' id='menu-toggle' />
+        <label htmlFor='menu-toggle' className='menu-icon'>
+          &#9776;
+        </label>
+        <ul className='menu'>
+          <li>
+            <Link to='#about'>About Me</Link>
+          </li>
+
+          <li>
+            <Link to='#projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to='#skills'>Skills</Link>
+          </li>
+          <li>
+            <Link to='#contact' className='navbar-btn'>
+              Contact Me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function home() {
+  return (
+    <div>
+      <h3>Hello Everyone!</h3>
+      <>
+        <p>
+          My name is Ruben. <span> {`I'm a full stack developer`}</span>
+        </p>
+      </>
+    </div>
+  );
+}
+
+export default home;
