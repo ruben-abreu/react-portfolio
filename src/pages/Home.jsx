ec7e275094ae52d57cae83c1adcb316d4f7fe81e
Ruben M. Abreu <ruben_abreu@icloud.com>
2023-12-15 00:13:32 +0000

    adds material-ui and final details to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,63 @@
 import avatar from '../assets/Screenshot 2023-12-14 at 17.36.03.png';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ArticleIcon from '@mui/icons-material/Article';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
+  const [text, setText] = useState("I'm a full stack developer.");
+  const [displayText, setDisplayText] = useState('');
+
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setDisplayText(text.substring(0, i));
+      i++;
+      if (i > text.length) {
+        clearInterval(interval);
+      }
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, [text]);
+
   return (
     <div className='flex items-center justify-center h-[80vh]'>
       <div className='grid grid-cols-2 gap-4'>
-        <div className='text-center text-xl'>
+        <div className='text-center md:text-xl flex flex-col justify-center items-center h-full'>
           <div>
             <h3>Hello Everyone!</h3>
           </div>
           <p>
-            My name is Ruben. <span> {`I'm a full stack developer`} </span>
+            {`My name is Ruben, `}
+            <span className='  text-[#4CB9E7] shadow-custom '>
+              {displayText}
+            </span>
           </p>
-          <div className='flex items-center md:mt-24'>
-            <button className='navbar-btn'>My CV</button>
+          <div className='md:flex items-center justify-center gap-4 md:mt-24'>
+            <button className='navbar-btn'>
+              <ArticleIcon className='mr-1' />
+              My CV
+            </button>
             <a
               href='https://www.linkedin.com/in/ruben-abreu1/'
               target='_blank'
               rel='noopener noreferrer'
             >
-              <button className='navbar-btn'>LinkedIn</button>
+              <button className='navbar-btn'>
+                <LinkedInIcon className='mr-1' />
+                LinkedIn
+              </button>
             </a>
             <a
               href='https://github.com/ruben-abreu'
               target='_blank'
               rel='noopener noreferrer'
             >
-              <button className='navbar-btn'>Github</button>
+              <button className='navbar-btn'>
+                <GitHubIcon className='mr-1' />
+                Github
+              </button>
             </a>
           </div>
         </div>
